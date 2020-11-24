3d3d84591f8aff4ea9e495bd2287ea2015b15fe4
EvelynD95 <evelyn.dantas95@gmail.com>
2020-11-24 16:44:10 -0300

    Parte estática pronta, estilo básico adicionado

---
+++ pokedex/src/components/HomePage/HomePage.js
@@ -0,0 +1,188 @@
+import React from 'react'
+import NavBar from '../NavBar'
+import { ButtonContainer, CardContainer, ImageCardContainer, MainContainer } from './styles';
+
+function HomePage() {
+  return (
+    <div>
+      <NavBar/>
+      <MainContainer>
+        <CardContainer>
+            <ImageCardContainer>
+                <img src='https://picsum.photos/70/?random=1'/>
+            </ImageCardContainer>
+            <ButtonContainer>
+                <button>Adicionar a pokédex</button>
+                <button>Ver detalhes</button>
+            </ButtonContainer>
+          </CardContainer>
+          <CardContainer>
+            <ImageCardContainer>
+                <img src='https://picsum.photos/70/?random=2'/>
+            </ImageCardContainer>
+            <ButtonContainer>
+                <button>Adicionar a pokédex</button>
+                <button>Ver detalhes</button>
+            </ButtonContainer>
+          </CardContainer><CardContainer>
+            <ImageCardContainer>
+                <img src='https://picsum.photos/70/?random=3'/>
+            </ImageCardContainer>
+            <ButtonContainer>
+                <button>Adicionar a pokédex</button>
+                <button>Ver detalhes</button>
+            </ButtonContainer>
+          </CardContainer><CardContainer>
+            <ImageCardContainer>
+                <img src='https://picsum.photos/70/?random=4'/>
+            </ImageCardContainer>
+            <ButtonContainer>
+                <button>Adicionar a pokédex</button>
+                <button>Ver detalhes</button>
+            </ButtonContainer>
+          </CardContainer><CardContainer>
+            <ImageCardContainer>
+                <img src='https://picsum.photos/70/?random=5'/>
+            </ImageCardContainer>
+            <ButtonContainer>
+                <button>Adicionar a pokédex</button>
+                <button>Ver detalhes</button>
+            </ButtonContainer>
+          </CardContainer><CardContainer>
+            <ImageCardContainer>
+                <img src='https://picsum.photos/70/?random=6'/>
+            </ImageCardContainer>
+            <ButtonContainer>
+                <button>Adicionar a pokédex</button>
+                <button>Ver detalhes</button>
+            </ButtonContainer>
+          </CardContainer><CardContainer>
+            <ImageCardContainer>
+                <img src='https://picsum.photos/70/?random=7'/>
+            </ImageCardContainer>
+            <ButtonContainer>
+                <button>Adicionar a pokédex</button>
+                <button>Ver detalhes</button>
+            </ButtonContainer>
+          </CardContainer><CardContainer>
+            <ImageCardContainer>
+                <img src='https://picsum.photos/70/?random=8'/>
+            </ImageCardContainer>
+            <ButtonContainer>
+                <button>Adicionar a pokédex</button>
+                <button>Ver detalhes</button>   
+            </ButtonContainer>
+          </CardContainer><CardContainer>
+            <ImageCardContainer>
+                <img src='https://picsum.photos/70/?random=9'/>
+            </ImageCardContainer>
+            <ButtonContainer>
+                <button>Adicionar a pokédex</button>
+                <button>Ver detalhes</button>
+            </ButtonContainer>
+          </CardContainer><CardContainer>
+            <ImageCardContainer>
+                <img src='https://picsum.photos/70/?random=10'/>
+            </ImageCardContainer>
+            <ButtonContainer>
+                <button>Adicionar a pokédex</button>
+                <button>Ver detalhes</button>
+            </ButtonContainer>
+          </CardContainer>
+          <CardContainer>
+            <ImageCardContainer>
+                <img src='https://picsum.photos/70/?random=11'/>
+            </ImageCardContainer>
+            <ButtonContainer>
+                <button>Adicionar a pokédex</button>
+                <button>Ver detalhes</button>
+            </ButtonContainer>
+          </CardContainer>
+          <CardContainer>
+            <ImageCardContainer>
+                <img src='https://picsum.photos/70/?random=12'/>
+            </ImageCardContainer>
+            <ButtonContainer>
+                <button>Adicionar a pokédex</button>
+                <button>Ver detalhes</button>
+            </ButtonContainer>
+          </CardContainer>
+          <CardContainer>
+            <ImageCardContainer>
+                <img src='https://picsum.photos/70/?random=13'/>
+            </ImageCardContainer>
+            <ButtonContainer>
+                <button>Adicionar a pokédex</button>
+                <button>Ver detalhes</button>
+            </ButtonContainer>
+          </CardContainer>
+          <CardContainer>
+            <ImageCardContainer>
+                <img src='https://picsum.photos/70/?random=14'/>
+            </ImageCardContainer>
+            <ButtonContainer>
+                <button>Adicionar a pokédex</button>
+                <button>Ver detalhes</button>
+            </ButtonContainer>
+          </CardContainer>
+          <CardContainer>
+            <ImageCardContainer>
+                <img src='https://picsum.photos/70/?random=15'/>
+            </ImageCardContainer>
+            <ButtonContainer>
+                <button>Adicionar a pokédex</button>
+                <button>Ver detalhes</button>
+            </ButtonContainer>
+          </CardContainer>
+          <CardContainer>
+            <ImageCardContainer>
+                <img src='https://picsum.photos/70/?random=16'/>
+            </ImageCardContainer>
+            <ButtonContainer>
+                <button>Adicionar a pokédex</button>
+                <button>Ver detalhes</button>
+            </ButtonContainer>
+          </CardContainer>
+          <CardContainer>
+            <ImageCardContainer>
+                <img src='https://picsum.photos/70/?random=17'/>
+            </ImageCardContainer>
+            <ButtonContainer>
+                <button>Adicionar a pokédex</button>
+                <button>Ver detalhes</button>
+            </ButtonContainer>
+          </CardContainer>
+          <CardContainer>
+            <ImageCardContainer>
+                <img src='https://picsum.photos/70/?random=18'/>
+            </ImageCardContainer>
+            <ButtonContainer>
+                <button>Adicionar a pokédex</button>
+                <button>Ver detalhes</button>
+            </ButtonContainer>
+          </CardContainer>
+          <CardContainer>
+            <ImageCardContainer>
+                <img src='https://picsum.photos/70/?random=19'/>
+            </ImageCardContainer>
+            <ButtonContainer>
+                <button>Adicionar a pokédex</button>
+                <button>Ver detalhes</button>
+            </ButtonContainer>
+          </CardContainer>
+          <CardContainer>
+            <ImageCardContainer>
+                <img src='https://picsum.photos/70/?random=20'/>
+            </ImageCardContainer>
+            <ButtonContainer>
+                <button>Adicionar a pokédex</button>
+                <button>Ver detalhes</button>
+            </ButtonContainer>
+          </CardContainer>
+        </MainContainer>
+    </div>
+       
+  );
+}
+
+export default HomePage;
