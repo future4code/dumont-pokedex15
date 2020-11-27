a299d566dc3027ac0d062821438654035a6ae40a
fefealfonsi <fefealfonsi@gmail.com>
2020-11-27 14:20:57 -0300

    DetailsPage funcionando

--- pokedex/src/pages/PokeDetails/PokeDetailsPage.js
+++ pokedex/src/pages/PokeDetails/PokeDetailsPage.js
@@ -27,7 +27,7 @@ function PokeDetailsPage() {
     axios.get(`${BASE_URL}/pokemon/${id}`)
       .then((response)=>{
         setPokeDetail(response.data)
-        
+        console.log(response.data)
       }).catch((error)=>{
         console.log(error)
       })
@@ -38,24 +38,47 @@ function PokeDetailsPage() {
       <NavBar/>
     {pokeDetail && (
       <div>
-        
-          <ContainerPokeName>
-            <h1>{pokeDetail.name}</h1>
-          </ContainerPokeName>
-                      
-            <DetailCardPhoto >
-                <img src={pokeDetail.sprites.front_default} />
-            </DetailCardPhoto>
-            <DetailCardPhoto >
-                <img src={pokeDetail.sprites.back_default} />
-            </DetailCardPhoto>
+       <ContainerPokeName>
+       <h1>{pokeDetail.name}</h1>
+     </ContainerPokeName>
+      <ContainerDetails>
+            <ContainerImg>
+              <DetailCardPhoto >
+                  <img src={pokeDetail.sprites.front_default} />
+              </DetailCardPhoto>
+              <DetailCardPhoto >
+                  <img src={pokeDetail.sprites.back_default} />
+              </DetailCardPhoto>
+            </ContainerImg>          
+            
             <DetailCardInfo>
+              <h4>Status</h4>
               {pokeDetail.stats.map((pokemon)=>{
                 return <p>{pokemon.stat.name}: {pokemon.base_stat}</p>
               })}
             </DetailCardInfo>
-          
+            <ContainerTypeMove>
+              <DetailCardTypes>
+              {pokeDetail.types.map((pokemon)=>{
+                return <p>Tipo: {pokemon.type.name}</p>
+              })}
+              </DetailCardTypes>
+                
+              <DetailCardMoves>
+                <h4>Moves</h4>
+                {pokeDetail.moves.map((pokemon,i)=>{
+                  if(i<5){
+                    return <p>{pokemon.move.name}</p>
+                  }else{
+
+                  }
+                
+              })}
+              </DetailCardMoves>  
+            </ContainerTypeMove>
+            
         
+        </ContainerDetails>
         </div>
     )}
   </div>
